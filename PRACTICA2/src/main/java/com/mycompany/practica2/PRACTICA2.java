3f179312c00eee32ca917755fa3a47a71d21c926
alexr <alexrobles084@gamil.com>
2024-10-18 16:04:49 -0500

    Proyecto creado

---
+++ PRACTICA2/src/main/java/com/mycompany/practica2/PRACTICA2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica2;
+
+/**
+ *
+ * @author alexr
+ */
+public class PRACTICA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
